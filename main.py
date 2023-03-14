2b653c4f6d64880038a250be4de087fd3461aef3
krak_os <48405896+missedpoints@users.noreply.github.com>
2023-03-14 06:59:14 +0000

    main

---
+++ main.py
@@ -0,0 +1,24 @@
+## math.
+import math
+import matlab
+import matplotlib
+##i = moment of intertia. FIND MOMENT OF INERTIA!
+youngs = (10e5)*2.1
+
+##################
+full_length = 88##
+beam_width = 7#### these are recorded in millimeters.
+beam_depth = 8####
+##################
+simple_i = (beam_width * beam_depth**3)/12
+cantilever_i = 0 ## TODO: Cantilever's moment of inertia.
+fixed_i = 0 ##TODO: Fixed beam's moment of inertia.
+
+simply_maximum_deflection = 164
+
+def main():
+    print()
+
+
+
+main()
