5586d99e537f7573456e2d305ca0d0376789419a
krak_os <48405896+missedpoints@users.noreply.github.com>
2023-03-16 17:43:35 +0000

    reformatted

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 import math
 import matlab
 import matplotlib
-##i = moment of intertia. FIND MOMENT OF INERTIA!
-e = 2.11e5 ## in ksi ## Young's Modulus for Carbon Steel.
+# i = moment of intertia. FIND MOMENT OF INERTIA!
+e = 2.11e5  # in ksi ## Young's Modulus for Carbon Steel.
 
 #####################
-full_length = 88##### length of bar
-beam_width = 7####### these are recorded in millimeters.
-beam_depth = 8**3#### depth of bar.
+full_length = 88  # length of bar
+beam_width = 7  # these are recorded in millimeters.
+beam_depth = 8  # depth of bar.
 #####################
 
-moment_of_inertia = (beam_width * (beam_depth))/12
+moment_of_inertia = (beam_width * (beam_depth**3))/12
 
 simply_maximum_deflection = 164
 
-simple_moment_of_inertia = (beam_width*(beam_depth))/12
+simple_moment_of_inertia = (beam_width*(beam_depth**3))/12
 
-simple_maximum_perm_deflection = (164*(full_length**3))/48*e*simple_moment_of_inertia
+simple_maximum_perm_deflection = (
+    164*(full_length**3))/48*e*simple_moment_of_inertia
+
 
 def main():
-    print("Permenant deflection is.. ",simple_maximum_perm_deflection)
-    print("Simple moment of inertia..",simple_moment_of_inertia,"mm^4")
-    print(beam_depth,beam_depth**-3)
+    print("Permenant deflection is.. ", simple_maximum_perm_deflection)
+    print("Simple moment of inertia..", simple_moment_of_inertia, "mm^4")
+    print(beam_depth, beam_depth)
     print(e)
+
 
 main()
