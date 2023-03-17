05c87ba3396d5370a9ccdab9032f22b327bd3bac
krak_os <48405896+missedpoints@users.noreply.github.com>
2023-03-17 12:57:02 +0000

    going absolutely mental at this point

--- main.py
+++ main.py
@@ -17,31 +17,28 @@ fixed_maximum_perm_deflection = (250*(full_length**3))/192*e*moment_of_inertia
 ######################################################################################
 
 displacements = [0,2,4,6,8,10,12,14,16,18,20,22,24,26] ## kind of useless, couldn't find a use for it.
-cantilever_applied_loads = [] 
-fixed_applied_loads = []
-simple_applied_loads = []
+cantilever_applied_loads = [180,160,140,120,100,80,60,40,20,0]
+fixed_applied_loads = [200,180,160,140,120,100,80,60,40,20,0] ## Put your applied loads here; or leave as-is for my results.
+simple_applied_loads = [164,140,120,100,80,60,40,20,0]
 
 
 def Simple_Bending_Moment(applied_loads,L):
     for load in applied_loads:
         M_max = (load * L) / 4
         print(f"Applied Load: {load} N, Maximum Bending Moment: {M_max} Nm")
-        Hookes_Law(M_max)
+        print(f"Theoretical stress for Simple is: {(M_max*(full_length/2))/e}M.")
 
 def Cantilever_Bending_Moment(applied_loads,L):
     for load in applied_loads:
         M_max = (load * (L**2)) / 8
         print(f"Applied Load: {load} N, Maximum Bending Moment: {M_max} Nm")
-        Hookes_Law(M_max)
+        print(f"Theoretical stress for Cantilever is: {(M_max*(full_length/2))/e}M.")
 
 def Fixed_Bending_Moment(applied_loads,L):
     for load in applied_loads:
         M_max = (load * (L**2)) / 16
         print(f"Applied Load: {load} N, Maximum Bending Moment: {M_max} Nm")
-        Hookes_Law(M_max)
-
-def Hookes_Law(bendingmoment,L):
-    return (bendingmoment*(full_length/2))/e
+        print(f"Theoretical stress for Fixed is: {(M_max*(full_length/2))/e}M.")
 
 
 def main():
@@ -50,5 +47,8 @@ def main():
     print(f"Cantilever maximum deflection: {cantilever_maximum_perm_deflection}m")
     print(f"Fixed maximum deflection: {fixed_maximum_perm_deflection}m")
     print(f"Modulus (E): {e}",  "Gigapascals")
+    Simple_Bending_Moment(simple_applied_loads,full_length)
+    Cantilever_Bending_Moment(cantilever_applied_loads,full_length)
+    Fixed_Bending_Moment(fixed_applied_loads,full_length)
 
 main()
